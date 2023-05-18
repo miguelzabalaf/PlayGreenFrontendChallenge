9b5f4aabe26c729ba341435b9f6d7d443b345be9
Miguel <miguel@192.168.1.6>
2023-05-17 23:47:08 -0500

    fix: add redirect to auth routes

--- src/router/constants.ts
+++ src/router/constants.ts
@@ -2,21 +2,21 @@ export const routes = {
     dashboard: {
         path: '/dashboard/*',
         children: {
-            home: { path: 'home' },
+            home: { path: 'home', fullPath: '/dashboard/home' },
             history: { path: 'history' },
         },
     },
     auth: {
         path: '/auth/*',
         children: {
-            login: { path: 'login' },
+            login: { path: 'login', fullPath: '/auth/login' },
         }
     },
     common: {
         path: '/*',
         children: {
-            notFound: { path: '/404' },
-            logout: { path: '/logout' },
+            notFound: { path: '/404', fullPath: '/404' },
+            logout: { path: '/logout', fullPath: '/logout' },
         }
     }
 };
